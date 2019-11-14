5f60814a30d1170377601a101870d1f1d328a11a
Paul <paul@paulgoesdeep.com>
2019-11-14 21:38:48 +0300

    Spinup ignores directory parameter — fixed

--- lib/spinup/runner.rb
+++ lib/spinup/runner.rb
@@ -1,10 +1,10 @@
 module Spinup
   class Runner
-    def initialize(playground, _dir)
+    def initialize(playground, dir)
       default_dir = File.expand_path(Spinup::Config::DEFAULT_DIR)
       session_key = SecureRandom.hex[0..6]
 
-      directory ||= Locator.new(default_dir).(playground, session_key)
+      directory = dir || Locator.new(default_dir).(playground, session_key)
       config = Config::PLAYGROUNDS[playground]
 
       builder = PlaygroundBuilder.new(directory, Opener.new)
